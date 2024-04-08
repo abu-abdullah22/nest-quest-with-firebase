4ee24a9d9b4d3cc48d0d94285dfd7da5325fbd2b
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-04-08 16:56:28 +0600

    estate details created

--- src/Components/Estate.jsx
+++ src/Components/Estate.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Estate = ({data}) => {
     return (
         <div className="card card-compact bg-base-100 shadow-xl mx-5 hover:scale-105 hover:border-2 border-gray-400 hover:cursor-pointer">
@@ -8,7 +10,7 @@ const Estate = ({data}) => {
           <p className="font-medium text-lg text-[#131313CC]">{data.location}</p>
           <p className="text-xl font-bold text-[#131313CC]">Status : {data.status}</p>
           <div className="card-actions ">
-            <button className="btn btn-accent">{data.button}</button>
+          <Link to={`/estate/${data.id}`}> <button className="btn btn-accent">{data.button}</button></Link>
           </div>
         </div>
       </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home";
 import UpdateProfile from "../Pages/UpdateProfile";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
+import EstateDetails from "../Pages/EstateDetails";
 
 export const router = createBrowserRouter([
     {
@@ -26,6 +27,11 @@ export const router = createBrowserRouter([
         {
           path: "/register",
           element : <Register></Register>
+        },
+        {
+          path: '/estate/:id',
+          element: <EstateDetails></EstateDetails>,
+          loader : () => fetch('/data.json')
         }
       ]
     },
